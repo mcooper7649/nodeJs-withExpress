7ae5919619c2e683a2179fc9f44ec29654482605
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-24 15:01:02 -0500

    completed get, req, res modules

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send("<h1>Hello, Everybody</h1>");
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
