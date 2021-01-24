da562d7184f7fef6b2a3ad97eb1dea969cc449c9
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-24 15:18:37 -0500

    Completed Routes Lesson and Challenge

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,10 +1,25 @@
+// Declarations
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+//Routes
+
 app.get('/', (req, res) => {
     res.send("<h1>Hello, Everybody</h1>");
   })
+app.get('/contact', (req, res) => {
+    res.send("<h1>Contact me, at test@gmail.com</h1>");
+  })
+app.get('/about', (req, res) => {
+    res.send(
+        "<h2>The author of this page, Mike Cooper, Enjoys Coding and Video Games.</h2>"
+    )
+})
+
+
+//Listenening Ports
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
